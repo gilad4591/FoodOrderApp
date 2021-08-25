b7f2b155235637cb75fa936645cfb2b382fd7194
Gilad Cohen <gilad4591@gmail.com>
2021-08-26 00:36:42 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import Meals from './components/Meals/Meals';
+import Header from './components/Layout/Header';
+import Cart from './components/Cart/Cart';
+function App() {
+  return (
+    <Fragment>
+    <Cart/>
+<Header/>
+<main>
+<Meals/>
+</main>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return <React.Fragment>
+      <header className={classes.header}>
+          <h1>Food App</h1>
+          <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+          <img src={mealsImage} alt="Food Table"/>
+      </div>
+  </React.Fragment>;
+};
+
+export default Header;
